a445fda0eb684e2d34396f72d0bb35987f59781e
Khaldrogo0 <vnn924@gmail.com>
2019-01-14 16:58:57 +0530

    Updates

---
+++ precache-manifest.bb56b8a52b251f0a451eef859b605d34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "1d061d41129b07756536",
+    "url": "/my-app/static/js/main.1d061d41.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/my-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1d061d41129b07756536",
+    "url": "/my-app/static/css/main.9d4076ca.chunk.css"
+  },
+  {
+    "revision": "a851fffd5e4a89690a2f181980b65de4",
+    "url": "/my-app/index.html"
+  }
+];
