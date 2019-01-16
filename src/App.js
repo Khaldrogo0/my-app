06b9f4e132a08a1fc46eb73ba197cdc8d7940b11
Vaishakhnath <vnn924@gmail.com>
2019-01-16 14:29:14 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/person';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Person/>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
