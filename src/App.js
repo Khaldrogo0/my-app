edbf9625eeaef22d52f90527d8b77c85141385d8
Vaishakhnath <vnn924@gmail.com>
2019-01-21 00:34:40 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Person from './Person/person';
+
+import Validation from './assignment/Validation';
+import Char from './assignment/Char';
 
 class App extends Component {
+  state = {
+    userin:""
+  }
+  deletecharhandeler= (index) =>{
+    const text =this.state.userin.split('');
+    text.splice(index, 1);
+    const updatedtext = text.join('');
+    this.setState({userin : updatedtext});
+    }
+
+ 
+  lengthhandler = (event) => {
+    this.setState({userin:event.target.value});
+  }
   render() {
+
+    const comp= this.state.userin.split('').map((ch, index) => {
+    return <Char 
+    character={ch} 
+    key={index}
+    del={ () => this.deletecharhandeler(index)}/>;
+  
+  });
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Person/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+          <input type="text" onChange={this.lengthhandler} value={this.state.userin} />
+          <p>{this.state.userin}</p>
+          </div>
+          <Validation inputlen={this.state.userin.length}/>
+          {comp}     
         </header>
       </div>
     );
